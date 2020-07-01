032dd45398cf6618a4f6358f5ef0c42f4fa347c3
Zbda <koulkzbda@gmail.com>
2020-07-01 14:12:00 +0200

    tp2

--- src/main/java/fr/diginamic/TestConnexionJdbc.java
+++ src/main/java/fr/diginamic/TestConnexionJdbc.java
@@ -13,6 +13,8 @@ public class TestConnexionJdbc {
 
 		Class.forName(db.getString("database.driver"));
 		
+		// TP1
+		
         String url = db.getString("database.url");
         String user = db.getString("database.user");
         String pass =  db.getString("database.pass");
@@ -24,6 +26,23 @@ public class TestConnexionJdbc {
         } catch (SQLException e) {
         	System.out.println(e.getMessage());
         }
+        
+        //  TP2
+        
+        Class.forName(db.getString("db.driver"));
+        
+        String url2 = db.getString("db.MYSQL_ADDON_URI");
+        String user2 = db.getString("db.MYSQL_ADDON_USER");
+        String pass2 =  db.getString("db.MYSQL_ADDON_PASSWORD");
+        
+        try (
+        		Connection co2 = DriverManager.getConnection(url2, user2, pass2);
+        		) {
+        	System.out.println(co2);
+        } catch (SQLException e) {
+        	System.out.println(e.getMessage());
+        }
+        
 	}
 
 }
